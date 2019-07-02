e88f2ab787b8a3b6f3c8ca4d7719bd2450a1e60f
zaidi1997 <17053109@myrp.edu.sg>
2019-07-02 13:45:36 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demomap/MainActivity.java
@@ -0,0 +1,140 @@
+package sg.edu.rp.c346.demomap;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3;
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                fm.findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback(){
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                LatLng poi_CausewayPoint = new LatLng(1.436065, 103.786263);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_CausewayPoint,
+                        15));
+
+                UiSettings ui = map.getUiSettings();
+
+                ui.setCompassEnabled(true);
+                ui.setZoomControlsEnabled(true);
+
+                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
+                    map.setMyLocationEnabled(true);
+                } else {
+                    Log.e("GMap - Permission", "GPS access has not been granted");
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                }
+
+                Marker cp = map.addMarker(new
+                        MarkerOptions()
+                        .position(poi_CausewayPoint)
+                        .title("Causeway Point")
+                        .snippet("Shopping after class")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+
+                LatLng poi_RP = new LatLng(1.44224, 103.785733);
+                Marker rp = map.addMarker(new
+                        MarkerOptions()
+                        .position(poi_RP)
+                        .title("Republic Polytechnic")
+                        .snippet("C347 Android Programming II")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+
+            }
+        });
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null){
+                    map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                }
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null){
+                    map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                }
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null){
+                    map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay!
+                    Toast.makeText(MainActivity.this, "Permission granted",
+                            Toast.LENGTH_SHORT).show();
+
+                } else {
+                    // permission denied... notify user
+                    Toast.makeText(MainActivity.this, "Permission not granted",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+}
+
+
